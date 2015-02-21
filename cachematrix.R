96307b876f4c015e9978f28aaaa933556e4e2a15
Kesha <kochub@gmail.com>
2015-02-21 14:30:40 +0300

    Submitting working results for evaluation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Function creates a special "matrix", which is really a list containing a function to
+## 1.  set the value of the matrix
+## 2.  get the value of the matrix
+## 3.  set the value of the inverse matrix
+## 4.  get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+      
+    ## intiate values
+    inverted <- NULL
+      
+  	## set the value of the matrix
+  	set <- function(y = matrix()) {
+	    x <<- y
+	    inverted <<- NULL
+  	}
+      
+  	## get the value of the matrix
+  	get <- function() x
 
+	## set the value of the inverse matrix
+	setinvmatrix <- function(invMatrix) inverted <<- invMatrix
+
+	## get the value of the inverse matrix
+	getinvmatrix <- function() inverted
+	list(set = set, get = get,
+	   setinvmatrix = setinvmatrix,
+	   getinvmatrix = getinvmatrix)
 }
 
 
-## Write a short comment describing this function
+## function calculates the iverted version of the matrix created with the above function.
+## it first checks to see if the iversion has already been calculated. If so, it `get`s the value from the
+## cache and skips the computation.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	## getting value from provided list
+	inverted <- x$getinvmatrix()
+	
+	## check if we could bypass calculations
+	if(!is.null(inverted)) {
+	      message("getting cached data")
+	      return(inverted)
+	}
+
+	## getting data for calculations
+	data <- x$get()
+
+	## calculating actual data
+	inverted <- solve(data, ...)
+	
+	## puching result back to cache
+	x$setinvmatrix(inverted)
+	
+	## return result
+	inverted
 }
